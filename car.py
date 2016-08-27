00f65941c33d01d3caf6c6fed8db1c50aac9234b
Vitalik <si_vitalik@yahoo.com>
2016-08-27 13:59:00 +0300

    dict

--- car.py
+++ car.py
@@ -2,11 +2,12 @@ import random
 n = 10
 list1 = [random.randint(1, n) for i in range(n)]
 list2 = [random.randint(1, n) for i in range(n)]
-d1 = dict(zip(list1,list2))
-citys = {}
-for i in list1:
-    if i in citys:
-        citys[list1[i]] += list2[i]
+
+cities = {}
+for k, v in zip(list1,list2):
+    if k in cities:
+        cities[k].append(v) 
     else:
-        citys[list1[i]] = list2[i]
+        cities[k] = [v]
     
+print (cities)
