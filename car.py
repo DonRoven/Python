de3321b13086f6c17314cfa30467ebe93668c4a3
Vitalik <si_vitalik@yahoo.com>
2016-09-01 18:14:29 +0300

    Robocha pravka

--- car.py
+++ car.py
@@ -1,5 +1,9 @@
 import random
+
+city1 = 8
+city2 = 5
 n = 10
+
 list1 = [random.randint(1, n) for i in range(n)]
 list2 = [random.randint(1, n) for i in range(n)]
 
@@ -15,5 +19,28 @@ for k, v in zip(list1,list2):
             cities[v].append(k) 
         else:
             cities[v] = [k]
-    
-print (cities)
+
+def check_direct_path(city1, city2, list1, list2):
+    if x == list1[i] and y == list2[i] or x == list2[i] and y == list1[i]:
+        return True
+        print (city1, city2)
+    else:
+        return False
+
+
+def check_path(city1, city2, cities):
+    path = []
+    path += [city1]
+    if city1 == city2:
+        return path
+    if not cities.has_key(city1):
+        return None
+    for node in cities[city1]:
+        if node not in path:
+            newpath = find_pach(city1, city2, cities)
+            if newpath:
+                return newpath
+    return None
+
+
+print (check_path(8,5))
--- graf.py
+++ graf.py
@@ -21,7 +21,7 @@ def cities_path(list1, list2):
     return (cities)
 
 def check_direct_path(city1, city2, list1, list2):
-    if x == list1[i] and y == list2[i] or x == list2[i] and y == list1[i]:
+    if city1 == list1[i] and city2 == list2[i] or city1 == list2[i] and city2 == list1[i]:
         return True
         print (city1, city2)
     else:
@@ -29,14 +29,14 @@ def check_direct_path(city1, city2, list1, list2):
 
 def check_path(city1, city2, cities):
     path = []
-    path += [start]
-    if start == end:
+    path += [city1]
+    if city1 == city2:
         return path
-    if not graph.has_key(start):
+    if not cities.has_key(city1):
         return None
-    for node in graph[start]:
+    for node in cities[city1]:
         if node not in path:
-            newpath = find_pach(graf, start, end)
+            newpath = find_pach(graf, city1, city2)
             if newpath:
                 return newpath
     return None
