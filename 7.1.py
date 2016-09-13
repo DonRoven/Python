734443c360dff247469482989fb8ebf2ccd5f88f
Vitalik <si_vitalik@yahoo.com>
2016-09-13 16:01:42 +0300

    sphere vypravyv

--- 7.1.py
+++ 7.1.py
@@ -1,18 +1,24 @@
 import math
 
-class Sphere(r = 1.0, x = 0.0, y = 0.0, z = 0.0):
+class Sphere():
     
-    def get_volume(self, r):
-        return (4/3*math.pi*(r**3))
+    def __init__(self, r = 1, x = 0, y = 0, z = 0):
+        self.r = r
+        self.x = x
+        self.y = y
+        self.z = z
 
-    def get_square(self, r):
-        return (4*math.pi*(r**2))
+    def get_volume(self):
+        return ((4/3)*math.pi*(self.r**3))
+
+    def get_square(self):
+        return (4*math.pi*(self.r**2))
 
     def get_radius(self):
         return (self.r)
     
     def get_center(self):
-        print ((x, y, z))
+        return ((x, y, z))
 
     def set_radius(self, r):
         self.r = r
@@ -22,8 +28,8 @@ class Sphere(r = 1.0, x = 0.0, y = 0.0, z = 0.0):
         self.y = y
         self.z = z
 
-    def is_point_inside(x,y,z):
-        if -self.r <= x <= self.r and -self.r <= y <= self.r and -self.r <= z <= self.r :
-            print (True)
+    def is_point_inside(self, x, y, z):
+        if math.sqrt((self.x - x)**2 + (self.y - y)**2 + (self.z - z)**2) <= self.r**2:
+            return True
         else:
-            print (False)
+            return False
